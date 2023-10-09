8498c54acc22ec82b7885c399411210a056af6c1
smveenhuijsen <s.m.veenhuijsen@student.rug.nl>
2023-10-09 12:46:07 +0200

    added network draw

--- test.py
+++ test.py
@@ -1,5 +1,6 @@
 import csv
 import networkx as nx
+import matplotlib.pyplot as plt
 
 
 def load_graph(filename):
@@ -13,8 +14,13 @@ def load_graph(filename):
 
     return G
 
-G = load_graph("data/soc-redditHyperlinks-title.tsv")
+def draw_first_x(G, x):
+    nx.draw(G.subgraph(list(G.nodes)[:x]), with_labels=False, node_size=10)
+    plt.show()
 
+G = load_graph("data/soc-redditHyperlinks-title.tsv")
 
 print(G)
 print(nx.number_connected_components(G))
+
+draw_first_x(G, 1000)
