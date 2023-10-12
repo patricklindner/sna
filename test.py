ae9ac0265da1e1f9b1c73ca1dbd4b1c81e5af7da
smveenhuijsen <s.m.veenhuijsen@student.rug.nl>
2023-10-12 12:34:24 +0200

    edit

--- test.py
+++ test.py
@@ -27,12 +27,12 @@ def print_stats(G):
     out_degree_sequence = [d for n, d in G.out_degree()]
 
     print(f"degree_sequence: {sum(degree_sequence)}")
-    print(f"in_degree_sequence: {sum(in_degree_sequence)}")
-    print(f"out_degree_sequence: {sum(out_degree_sequence)}")
+    print(f"in_degree_sequence: {G.in_degree()}")
+    print(f"out_degree_sequence: {G.out_degree()}")
 
-    degree_centrality = nx.degree_centrality(G)
-    betweenness_centrality = nx.betweenness_centrality(G)
-    closeness_centrality = nx.closeness_centrality(G)
+    degree_centrality = dict(sorted(nx.degree_centrality(G).items(), key=lambda item: item[1], reverse=True))
+    betweenness_centrality = dict(sorted(nx.betweenness_centrality(G).items(), key=lambda item: item[1], reverse=True))
+    closeness_centrality = dict(sorted(nx.closeness_centrality(G).items(), key=lambda item: item[1], reverse=True))
 
     print(f"degree_centrality: {degree_centrality}")
     print(f"betweenness_centrality: {betweenness_centrality}")
@@ -41,11 +41,14 @@ def print_stats(G):
     print(f"clustering_coefficient: {nx.average_clustering(G)}")
     # print(f"diameter: {nx.diameter(G)}")
     print(f"density: {nx.density(G)}")
-    print(f"number of connected components: {nx.number_connected_components(G)}")
+    print(f"number weakly connected components: {len(list(nx.weakly_connected_components(G)))}")
+    print(f"number strongly connected components: {len(list(nx.strongly_connected_components(G)))}")
 
-    # connected_components = list(nx.connected_components(G))
+    undirected_G = G.to_undirected(reciprocal=True)
+    connected_components = list(nx.connected_components(undirected_G))
     component_sizes = [len(component) for component in connected_components]
-    print(f"sizes of connected components: {component_sizes}")
+    print(f"connected components: {connected_components}")
+    print(f"sizes of connected components: {sorted(component_sizes, reverse=True)}")
 
 G = load_graph("data/soc-redditHyperlinks-title.tsv")
 
